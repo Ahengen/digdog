349bd64f7cdb8c0282818b46ff71301654c3ecb9
ahengen <ahengen92@gmail.com>
2021-05-02 20:15:22 -0500

    added to project

--- code.js
+++ code.js
@@ -24,28 +24,55 @@ for (let i = 0; i < NUM_BONES; i++) {
         console.log("re-roll: " + n);
     }
     $("span#" + n).addClass("bone");
-
 }
+let clicked_bones = 0;
+let gauge_value = 0.0;
+
 
 $("span.bone").on("click", function() {
    $(this).addClass("surprise");
+    clicked_bones++;
+    if (clicked_bones >= NUM_BONES) {
+        console.log("You have found all of the bones, good doggo!")
+    }
 });
 
 $("span").on("click", function() {
     $(this).addClass("dug");
+    $(this).off();
+    let min = 1/(NUM_BONES * NUM_BONES);
+    let max = 4/(NUM_BONES * NUM_BONES);
+    let diff = max - min;
+    let randnum = Math.random() * diff + min;
+    gauge_value += randnum;
+    setGaugeValue(gaugeElement, gauge_value);
+    if (gauge_value >= 1) {
+        console.log("Oh no! Too many holes!")
+    }
 });
 
-/*let changeSquare = $('#Brown');
+const gaugeElement = document.querySelector(".gauge");
 
-changeSquare.on('click', function() {
-    $(this).css({backgroundColor: 'brown'});
-})
-//Once the pristine yard has been constructed, hide your bones.
+function setGaugeValue(gauge, value) {
+    if (value > 1) {
+        value = 1;
+    }
+    if (value < 0 || value > 1) {
+        return;
+    }
+    gauge.querySelector(".gauge_fill").style.transform = `rotate(${
+        value / 2
+    }turn)`;
+    gauge.querySelector(".gauge_cover").textContent = `${Math.round(
+        value * 100
+    )}%`;
+}
+setGaugeValue(gaugeElement, gauge_value);
 
-/*Repeat picking a random square and hiding a bone in it util you
-have hidden num bones*/
 
-/*When picking random squares to hide bones, if one already has
-a bone in it pick again until an empty square is found to place the
-bone*/
+
+
+
+
+
 
